f807a75c6b35e674bf851be981de593498789ff0
elrenoel <elrenohasani@gmail.com>
2025-12-05 18:04:57 +0700

    api

--- app/lib/api_config.dart
+++ app/lib/api_config.dart
@@ -1,5 +1,5 @@
 class ApiConfig {
-  static const String baseUrl = "http://10.0.2.2/API_restaurant_app";
+  static const String baseUrl = "";
 
   static String countTables = "$baseUrl/count_table_available.php";
   static String login = "$baseUrl/login.php";
